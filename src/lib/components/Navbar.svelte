a5e8e57b33386dbb2ce263bca07a0f87a3834923
alinaswe45@gmail.com <85223818+Alinaswe3@users.noreply.github.com>
2024-01-18 23:45:10 +0200

    Fixed navbar toggle button

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,9 +1,27 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+
+    let isNavOpen = false;
+</script>
+
 <div class="navbar bg-base-100">
     <div class="navbar-start">
         <div class="dropdown">
-            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </div>
+            <button tabindex="0" class="btn btn-ghost lg:hidden" on:click={ e => {
+                if (isNavOpen) {
+                    const el = document.activeElement;
+                    if (el) {
+                        el?.blur();
+                    }
+                }
+                isNavOpen = !isNavOpen;
+            }}>
+                {#if isNavOpen}
+                    <Icon icon="fontisto:close-a"/>
+                {:else}
+                    <Icon icon="pajamas:hamburger" />
+                {/if}
+            </button>
             <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-btn w-52">
                 <li><a href="/">Home</a></li>
                 <li><a href="/">About</a></li>
