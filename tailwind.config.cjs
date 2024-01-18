bbc2a94906d22b8150ef67592c0ab5f7a879496d
alinaswe45@gmail.com <85223818+Alinaswe3@users.noreply.github.com>
2024-01-18 22:06:06 +0200

    Added default configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+    theme: {
+        extend: {}
+    },
 
-	plugins: []
+    plugins: [require("daisyui")],
+
+    daisyui: {
+        themes: [
+            {
+                custom: {
+                    "primary": "#00e472",
+                    "secondary": "#252829",
+                    "accent": "#abaeb0",
+                    "neutral": "#ffffff",
+                    "base-100": "#313536",
+                    "base-200": "#252829",
+                }
+            }, "cupcake", "retro"
+        ],
+        darkTheme: "dark", // name of one of the included themes for dark mode
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+        themeRoot: ":root", // The element that receives theme color CSS variables
+    },
 };
 
 module.exports = config;
