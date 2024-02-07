b06ad02163a162aab24bd2c35838b54dcda57976
alinaswe45@gmail.com <85223818+Alinaswe3@users.noreply.github.com>
2024-02-07 22:54:42 +0200

    Made an update to reviews

--- src/lib/components/Reviews.svelte
+++ src/lib/components/Reviews.svelte
@@ -6,11 +6,7 @@
 	<h3 class="section-title">what people saying</h3>
 	<h2 class="section-heading mt-8">Clients</h2>
 	<div class="responsive-grid">
-		<Review
-			clientImg="/images/chama.webp"
-			clientName="Chama Musonda"
-			serviceOffered="Business Client"
-		>
+		<Review clientImg="/images/chama.webp" clientName="Chama Musonda" serviceOffered="Customer">
 			I recently purchased the Dell XPS laptop from you, and I'm thoroughly impressed with its high
 			performance. The sleek design, powerful specs, and vibrant display make it a top-notch choice
 			for both work and entertainment. I Highly recommend TonyStar!
@@ -18,19 +14,16 @@
 		<Review
 			clientImg="/images/tasheni.webp"
 			clientName="Tasheni B. Chibwe"
-			serviceOffered="Business Client"
+			serviceOffered="Customer"
 		>
 			Tonystar Electronics lives up to their word. They deliver within their situated number of
 			days, and their products are always authentic and of high quality. I always buy from them and
 			have no regrets
 		</Review>
-		<Review
-			clientImg="/images/isabel.webp"
-			clientName="Isabel Cooke"
-			serviceOffered="Business Client"
-		>
+		<Review clientImg="/images/isabel.webp" clientName="Isabel Cooke" serviceOffered="Customer">
 			I love the iphone 11, the storage and all, the camera is great I have no complaints on
-			anything, I'd definitely recommend you
+			anything, I'd definitely recommend you. You always communicate to check if the phones are
+			alright, and if I'm satisfied with it
 		</Review>
 	</div>
 </section>
